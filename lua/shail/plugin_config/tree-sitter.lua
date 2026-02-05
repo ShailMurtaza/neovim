f63b7f65b87bba23bf9bafb0ce7a2b2f4742b297
ShailMurtaza <shailmurtaza@gmail.com>
2026-02-05 21:28:15 +0500

    Telescope: Install latest version.TreeSitter: Install Python by default.

--- lua/shail/plugin_config/tree-sitter.lua
+++ lua/shail/plugin_config/tree-sitter.lua
@@ -2,7 +2,7 @@ return {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     config = function()
-        require('nvim-treesitter.configs').setup {
+        require('nvim-treesitter.config').setup {
             ensure_installed = {
                 "javascript",
                 "typescript",
@@ -10,6 +10,7 @@ return {
                 "html",
                 "css",
                 "json",
+                "python",
             },
             highlight = {
                 enable = true,
